ea72753b3e33168c6b03af997c6da65a2c92d090
zeddrak <57454318+zeddrak@users.noreply.github.com>
2022-01-02 04:25:14 -0800

    Create masternuke.js

---
+++ masternuke.js
@@ -0,0 +1,33 @@
+/** @param {NS} ns **/
+import * as nt from "notns.js";
+
+let asd = {}; //all script data
+export async function main(ns) {
+	if (!Math.asd) { Math.asd = asd; } //if port's empty, initialize it
+	asd = Math.asd; //if port's not empty, populate asd
+
+	ns.disableLog('disableLog');
+	ns.disableLog('sleep');
+
+	ns.print("--Open Ports & Nuke 'em");
+	const servers = asd.servers.non??[];
+	for (const server of servers) {
+		if (!(server == 'home' || server.startsWith('pserv'))) {
+			const dat = ns.getServer(server);
+			if (dat.openPortCount < 5) {
+				if (!dat.sshPortOpen && ns.fileExists("BruteSSH.exe", "home")) { await ns.brutessh(server); }
+				if (!dat.ftpPortOpen && ns.fileExists("FTPCrack.exe", "home")) { await ns.ftpcrack(server); }
+				if (!dat.smtpPortOpen && ns.fileExists("relaySMTP.exe", "home")) { await ns.relaysmtp(server); }
+				if (!dat.httpPortOpen && ns.fileExists("HTTPWorm.exe", "home")) { await ns.httpworm(server); }
+				if (!dat.sqlPortOpen && ns.fileExists("SQLInject.exe", "home")) { await ns.sqlinject(server); }
+			}
+			if (!dat.hasAdminRights && dat.openPortCount >= dat.numOpenPortsRequired) {
+				ns.print('--NUKING ' + server);
+				await ns.nuke(server);
+				for (let i = 1; i<nt.sFiles.length; i+=2) {
+					await ns.scp(nt.sFiles[i],'home',server);
+				}
+			}
+		}
+	}
+}
