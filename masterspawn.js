0c77ad41a34732debe58fd6ea4699c7418945c1b
zeddrak <57454318+zeddrak@users.noreply.github.com>
2022-01-02 04:24:38 -0800

    Update masterspawn.js

--- masterspawn.js
+++ masterspawn.js
@@ -19,18 +19,17 @@ function launchAttack(ns, type, tar, threads = 1) {
 		const maxth = Math.max(0, Math.floor(freeRam(ns, res) / size))
 		if (maxth >= 1) {
 			//adjust threads needed if server has more cores
-			const coreMult = ((res == 'home') && (type == 'w' || type == 'g'))? asd.homeCoreMult : 1.0;
+			const coreMult = ((res == 'home') && (type == 'w' || type == 'g')) ? asd.homeCoreMult : 1.0;
 			const th = Math.min(Math.max(1, Math.ceil(threads / coreMult)), maxth); //threads to assign to this attack
 			const tixBool = (type == 'h' && asd.tixShort) || (type == 'g' && asd.tixLong); //manipulating the market?
-			const pid = ns.exec(script, res, th, tar, tixBool, Math.random());
+			const pid = ns.exec(script, res, th, tar, Math.random(), th, tixBool);
 			if (pid > 0) {
 				threads -= Math.floor(th * coreMult);
-				if (type == 'h') {hpids.push(pid);}
+				if (type == 'h') { hpids.push(pid); }
 			}
 		}
 	}
 }
-
 
 const MaxTargets = 1; //Maximum number of targets servers to atatck - currently only works for one, do not change
 
@@ -41,6 +40,8 @@ let asd = {}; //all script data
 export async function main(ns) {
 	if (!Math.asd) { Math.asd = asd; } //if port's empty, initialize it
 	asd = Math.asd; //if port's not empty, populate asd
+	asd.tixShort = asd.tixShort ?? false;
+	asd.tixLong = asd.tixShort ?? false;
 
 	ns.disableLog('disableLog');
 	ns.disableLog('sleep');
@@ -48,7 +49,7 @@ export async function main(ns) {
 	ns.disableLog('getServerMaxRam');
 	ns.disableLog('getServerUsedRam');
 	ns.disableLog('getServerMoneyAvailable');
-//	ns.disableLog('exec');
+	//	ns.disableLog('exec');
 	ns.disableLog('getServerSecurityLevel');
 	ns.clearLog();
 
@@ -56,59 +57,55 @@ export async function main(ns) {
 		await ns.sleep(0);
 
 		let totCost = 0; //add to this each time an active profile is added; up to totRam
-//		let totProc = 0; //add to this each time an active profile is added; up to MaxProcess
+		//		let totProc = 0; //add to this each time an active profile is added; up to MaxProcess
 		for (let i = 0; i < asd.bests.length && i < MaxTargets && totCost < asd.totRam; i++) { //ToDo: add totProc check
 			await ns.sleep(0);
-
 			try { ress = asd.servers.res; } catch { }
-			const startT = Date.now();
 
 			totCost += asd.bests[i].cost;
 			const tar = asd.bests[i].tar;
-			const bT = asd.bests[i].bT; // buffer time (range of time to land in) 
-			const cL = asd.bests[i].cL; // cycle length
 
-			const dat = asd.servers.dat[asd.servers.dat.indexOf(tar)+1];
+			const dat = asd.servers.dat[asd.servers.dat.indexOf(tar) + 1];
 			const hT = dat.hT; // length of a hack
 			const gT = dat.gT; // length of a grow
 			const wT = dat.wT; // length of a weaken
 
-			let wL = startT + 100; // weaken launch
-			let gL = wL + wT - gT - 10; //grow launch
-			let hL = gL + gT - hT - 10; //hack launch
-
-			const MonTol = (1.0 - asd.bests[i].amt) * 0.9 * dat.moneyMax; //don't allow more than 1 hack to hit
-			const SecTol = dat.minDifficulty + 1; //don't allow sec to rise by more than 1
-
-			let tL = Date.now(); //test server launch
+			let wL = Date.now() + 100; // weaken launch time
+			let gL = wL + wT - gT - 2 * asd.bests[i].cL - 10; //grow launch time
+			let hL = gL + gT - hT + 4 * asd.bests[i].cL - 10; //hack launch time
+			let tL = Date.now(); //test server launch time
 
 			let curTar = asd.bests[i].tar;
 			while (asd.bests[i].tar == curTar) { //will need to be removed if multiple targets are to be enabld
 				await ns.sleep(1);
+
+				const monTol = (1.0 - asd.bests[i].amt) * 0.90 * dat.moneyMax; //don't allow more than 1 hack to hit
+				const secTol = dat.minDifficulty + 1; //don't allow sec to rise by more than 1
+
 				//clean up old hPids
-				if (hpids.length > asd.bests[i].hP ) {
-					hpids.slice(0,hpids.length - asd.bests[i].hP);
+				if (hpids.length > asd.bests[i].hP) {
+					hpids.slice(0, hpids.length - asd.bests[i].hP);
 				}
 				//collission detection
-				if (Date.now() > tL && (ns.getServerSecurityLevel(tar) > SecTol || ns.getServerMoneyAvailable(tar) < MonTol )) {
-					for (let k = 0; k < 5 && hpids.length > 0; k++) { //kill a few of the oldest hacks
+				if (Date.now() > tL && (((ns.getServerSecurityLevel(tar) - secTol) > 0) || ((ns.getServerMoneyAvailable(tar) - monTol) < 0))) {
+					for (let i = 0; i<5 && hpids.length > 1; i++){
 						await ns.sleep(0);
-						while (hpids.length > 0 && !ns.kill(hpids.shift())) {await ns.sleep(0)} // kill until a hack is actually killed (or no hacks remain)
+						while (hpids.length > 0 && !ns.kill(hpids.shift())) { await ns.sleep(0) } // kill until a hack is actually killed (or no hacks remain)
+						tL = Date.now() + asd.bests[i].cL;
 					}
-					tL = Date.now() + bT;
 				}
 				try { ress = asd.servers.res; } catch { continue; }
 				if (Date.now() > wL) {
 					launchAttack(ns, 'w', tar, asd.bests[i].wN);
-					wL = Date.now() + bT;
+					wL = Date.now() + asd.bests[i].cL;
 				}
 				if (Date.now() > gL) {
 					launchAttack(ns, 'g', tar, asd.bests[i].gN);
-					gL = Date.now() + bT;
+					gL = Date.now() + asd.bests[i].cL;
 				}
-				if (Date.now() > hL){
+				if (Date.now() > hL) {
 					launchAttack(ns, 'h', tar, asd.bests[i].hN);
-					hL = Date.now() + cL;
+					hL = Date.now() + asd.bests[i].cL;
 				}
 			}
 		}
